0436c23a326eb38840ea946112fd7291daa48a38
Tobias Roland <samu1493@stud.kea.dk>
2022-05-30 14:27:58 +0200

    generel.js er oprettet

--- js/bestil.js
+++ js/bestil.js
@@ -1,7 +1,7 @@
-const burger = document.querySelector(".burger");
-const menu = document.querySelector(".nav-menu");
+// const burger = document.querySelector(".burger");
+// const menu = document.querySelector(".nav-menu");
 
-burger.addEventListener("click", () => {
-  burger.classList.toggle("active");
-  menu.classList.toggle("active");
-});
+// burger.addEventListener("click", () => {
+//   burger.classList.toggle("active");
+//   menu.classList.toggle("active");
+// });
